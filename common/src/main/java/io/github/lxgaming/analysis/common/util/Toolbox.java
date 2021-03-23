51d802203af08050a62b7d3841284b6fb49bf77d
Alex Thomson <LXGaming@users.noreply.github.com>
2021-03-23 20:08:39 +1300

    Add Profiler

--- common/src/main/java/io/github/lxgaming/analysis/common/util/Toolbox.java
+++ common/src/main/java/io/github/lxgaming/analysis/common/util/Toolbox.java
@@ -21,6 +21,7 @@ import com.google.gson.GsonBuilder;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 public class Toolbox {
     
@@ -30,6 +31,84 @@ public class Toolbox {
             .serializeNulls()
             .setPrettyPrinting()
             .create();
+    
+    public static String getDuration(long value, TimeUnit unit, boolean abbreviate, TimeUnit precision) {
+        StringBuilder stringBuilder = new StringBuilder();
+        append(stringBuilder, precision.convert(value, unit), precision, abbreviate);
+        return stringBuilder.toString();
+    }
+    
+    public static String getDuration(long value, TimeUnit unit, boolean abbreviate) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (TimeUnit.DAYS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toDays(value), TimeUnit.DAYS, abbreviate);
+        }
+        
+        if (TimeUnit.HOURS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toHours(value) % 24, TimeUnit.HOURS, abbreviate);
+        }
+        
+        if (TimeUnit.MINUTES.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toMinutes(value) % 60, TimeUnit.MINUTES, abbreviate);
+        }
+        
+        if (TimeUnit.SECONDS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toSeconds(value) % 60, TimeUnit.SECONDS, abbreviate);
+        }
+        
+        if (TimeUnit.MILLISECONDS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toMillis(value) % 1000, TimeUnit.MILLISECONDS, abbreviate);
+        }
+        
+        if (TimeUnit.MICROSECONDS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toMicros(value) % 1000, TimeUnit.MICROSECONDS, abbreviate);
+        }
+        
+        if (TimeUnit.NANOSECONDS.compareTo(unit) >= 0) {
+            append(stringBuilder, unit.toNanos(value) % 1000, TimeUnit.NANOSECONDS, abbreviate);
+        }
+        
+        return stringBuilder.toString();
+    }
+    
+    public static void append(StringBuilder stringBuilder, long value, TimeUnit unit, boolean abbreviate) {
+        if (value <= 0) {
+            return;
+        }
+        
+        if (stringBuilder.length() > 0) {
+            stringBuilder.append(abbreviate ? " " : ", ");
+        }
+        
+        stringBuilder.append(value);
+        if (!abbreviate) {
+            stringBuilder.append(" ");
+        }
+        
+        switch (unit) {
+            case NANOSECONDS:
+                stringBuilder.append(abbreviate ? "ns" : value == 1 ? "nanosecond" : "nanoseconds");
+                break;
+            case MICROSECONDS:
+                stringBuilder.append(abbreviate ? "\u03BCs" : value == 1 ? "microsecond" : "microseconds");
+                break;
+            case MILLISECONDS:
+                stringBuilder.append(abbreviate ? "ms" : value == 1 ? "millisecond" : "milliseconds");
+                break;
+            case SECONDS:
+                stringBuilder.append(abbreviate ? "s" : value == 1 ? "second" : "seconds");
+                break;
+            case MINUTES:
+                stringBuilder.append(abbreviate ? "min" : value == 1 ? "minute" : "minutes");
+                break;
+            case HOURS:
+                stringBuilder.append(abbreviate ? "h" : value == 1 ? "hour" : "hours");
+                break;
+            case DAYS:
+                stringBuilder.append(abbreviate ? "d" : value == 1 ? "day" : "days");
+                break;
+        }
+    }
     
     public static String getClassSimpleName(Class<?> type) {
         if (type.getEnclosingClass() != null) {
